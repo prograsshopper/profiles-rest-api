2d7152827b6fd1ba3492a15add6c21d2a03f9541
prograsshopper <tjalwled12@gmail.com>
2019-09-02 19:32:54 +0900

    Add custom user model and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
     
     def create_superuser(self, email, name, password):
         """Create and save a new super user with given details"""
-        user =  self._create_user(email, name, password)
+        user =  self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
